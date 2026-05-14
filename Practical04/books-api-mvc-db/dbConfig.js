b70a9864146cbc9a97af870ebf473bafe25e57ce
JanessaTan <s10272516@connect.np.edu.sg>
2026-05-14 10:38:41 +0800

    just saving

--- Practical04/books-api-mvc-db/dbConfig.js
+++ Practical04/books-api-mvc-db/dbConfig.js
@@ -1,11 +1,11 @@
 module.exports = {
-  user: "booksapi_user", // Replace with your SQL Server login username
-  password: "STOPtheSlop", // Replace with your SQL Server login password
-  server: "localhost",
-  database: "bed_db",
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  server: process.env.DB_SERVER,
+  database: process.env.DB_DATABASE,
   trustServerCertificate: true,
   options: {
-    port: 1433, // Default SQL Server port
+    port: parseInt(process.env.DB_PORT), // Default SQL Server port
     connectionTimeout: 60000, // Connection timeout in milliseconds
   },
 };
